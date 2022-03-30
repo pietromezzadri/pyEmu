91908fb27e4939e76df9592964d9c55b4044d6c4
Pietro Nardelli Mezzadri <pietromesadri@gmail.com>
2022-03-30 04:16:31 +0200

    testing memory module and GUI

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from memory.c8_memory import C8Memory
+from editor.chip_editor import ChipEditor
 
 mem  =  C8Memory()
 
 mem.clear_memory(False)
 
-print(len(mem.RAM))
-print(mem.RAM)
-print(mem.RAM[0x1FF])
-print(0x1FF)
+mem.RAM = [1]*mem.capacity
+
+print(f'size: {len(mem.RAM)}')
+
+mem.clear_memory()
+
+mem.display_info()
+print(mem.RAM[511])
+print(mem.RAM[512])
--- memory/c8_memory.py
+++ memory/c8_memory.py
@@ -1,17 +1,20 @@
 class C8Memory:
   def __init__(self) -> None:
+    self.id = 0x001
+    self.name = "Chip-8 Memory"
     self.capacity = 0xFFF
     self.reserved = [0x000, 0x1FF]
     self.start = 0x200
-    self.RAM = []
-    for i in range(self.capacity):
-      self.RAM.append(0)
+    self.RAM = [0] * self.capacity
+    self.read_only = False
 
-  def clear_memory(self, clear_reserved: bool = True) -> None:
-    if clear_reserved:
-      for index in range(self.capacity):
-        self.RAM[index] = 0
-      return
-    else:
-      for index in range(self.reserved[1]+1, self.capacity):
-        self.RAM[index] = 0
+  def clear_memory(self, clear_reserved: bool = False) -> None:
+    if not self.read_only:
+      if clear_reserved:
+        self.RAM = [0] * self.capacity
+        return
+      else:
+        self.RAM[self.reserved[1] + 1:] = [0] * (self.capacity - self.reserved[1] - 1)
+
+  def display_info(self) -> None:
+    print(f'ID: {self.id}\nCapacity: {self.capacity}\nReserved: {self.reserved[0]} | {self.reserved[1]}\nRead Only: {self.read_only}')
